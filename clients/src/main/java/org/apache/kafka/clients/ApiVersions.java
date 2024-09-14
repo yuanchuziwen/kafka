e8d32b527a7eb1cc60fbca955f58b20bf09b37a6
zhaowenbin <zhaowenbinbj@kanyun.com>
2024-09-14 10:52:35 +0800

    MOD：ApiVersions 增加注释

--- clients/src/main/java/org/apache/kafka/clients/ApiVersions.java
+++ clients/src/main/java/org/apache/kafka/clients/ApiVersions.java
@@ -27,28 +27,56 @@ import java.util.Optional;
 /**
  * Maintains node api versions for access outside of NetworkClient (which is where the information is derived).
  * The pattern is akin to the use of {@link Metadata} for topic metadata.
+ * 维护节点API版本信息，用于在 NetworkClient 之外访问。
+ * 该类的设计意图仅限于在 Kafka 内部使用。
  *
  * NOTE: This class is intended for INTERNAL usage only within Kafka.
+ * 注意：此类仅限于在 Kafka 内部使用。
  */
 public class ApiVersions {
 
+    /**
+     * 存储节点ID和对应的API版本信息
+     */
     private final Map<String, NodeApiVersions> nodeApiVersions = new HashMap<>();
+
+    /**
+     * 当前可用的最大生产者魔数
+     */
     private byte maxUsableProduceMagic = RecordBatch.CURRENT_MAGIC_VALUE;
 
+    /**
+     * 更新指定节点的API版本信息
+     * @param nodeId 节点ID
+     * @param nodeApiVersions 节点API版本信息
+     */
     public synchronized void update(String nodeId, NodeApiVersions nodeApiVersions) {
         this.nodeApiVersions.put(nodeId, nodeApiVersions);
         this.maxUsableProduceMagic = computeMaxUsableProduceMagic();
     }
 
+    /**
+     * 移除指定节点的API版本信息
+     * @param nodeId 要移除的节点ID
+     */
     public synchronized void remove(String nodeId) {
         this.nodeApiVersions.remove(nodeId);
         this.maxUsableProduceMagic = computeMaxUsableProduceMagic();
     }
 
+    /**
+     * 获取指定节点的API版本信息
+     * @param nodeId 节点ID
+     * @return 节点的API版本信息
+     */
     public synchronized NodeApiVersions get(String nodeId) {
         return this.nodeApiVersions.get(nodeId);
     }
 
+    /**
+     * 计算当前可用的最大生产者魔数
+     * @return 计算得到的最大生产者魔数
+     */
     private byte computeMaxUsableProduceMagic() {
         // use a magic version which is supported by all brokers to reduce the chance that
         // we will need to convert the messages when they are ready to be sent.
@@ -60,6 +88,10 @@ public class ApiVersions {
             knownBrokerNodesMinRequiredMagicForProduce.orElse(RecordBatch.CURRENT_MAGIC_VALUE));
     }
 
+    /**
+     * 获取当前可用的最大生产者魔数
+     * @return 当前可用的最大生产者魔数
+     */
     public synchronized byte maxUsableProduceMagic() {
         return maxUsableProduceMagic;
     }
